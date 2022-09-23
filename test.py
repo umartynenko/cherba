cf8347a1a7edc74102632ec6ff6623ed55f16114
umartynenko <uriy.martynenko@gmail.com>
2022-09-23 12:19:55 +0300

    Поиск ошибок

--- test.py
+++ test.py
@@ -1,3 +1,40 @@
+# def max(l):
+#     max = 0
+#     r = 0
+#
+# for i in range(len(l)):
+#     for j in range(len(l[0])):
+#         if l[i][j] > max:
+#             max = l[i][j]
+#
+# for i in range(len(l)):
+#     for j in range(len(l[0])):
+#         if l[i][j] = max:
+#             r = '(' + i + ';' + j + ')'
+#             print('Максимальный элемент:', max)
+#             return r
+#
+# n = int(input('Количество строк матрицы: '))
+# m = int(input('Количество столбцов матрицы: '))
+# h = []
+#
+# for i in range(n):
+#     h.append([])
+#     c = input()
+#     h[i] = c.split(' ')
+# for i in range(n):
+#     for j in range(m):
+#         h[i][j] = int(h[i][j])
+# print(max(l))
+## Пример правильного вывода:
+## Количество строк матрицы: 3
+## Количество столбцов матрицы: 5
+## 1 4 -9 3 2
+## 6 3 9 1 2
+## 9 0 5 8 -11
+## Максимальный элемент: 9
+## (2;3)
+
 def max(l):
     max = 0
     r = 0
