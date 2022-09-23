549007f3e9550d03c2c58f6350c4c21c16211f7a
umartynenko <uriy.martynenko@gmail.com>
2022-09-23 12:00:09 +0300

    Поиск ошибок

---
+++ test.py
@@ -0,0 +1,30 @@
+def max(l):
+    max = 0
+    r = 0
+    for i in range(len(l)):
+        for j in range(len(l[0])):
+            if int(l[i][j]) > max:
+                max = l[i][j]
+
+    for i in range(len(l)):
+        for j in range(len(l[0])):
+            if l[i][j] == max:
+                r = '(' + str(i + 1) + ';' + str(j + 1) + ')'
+                print('Максимальный элемент:', max)
+                return r
+
+
+n = int(input('Количество строк матрицы: '))
+m = int(input('Количество столбцов матрицы: '))
+h = []
+
+for i in range(n):
+    h.append([])
+    c = input()
+    h[i] = c.split(' ')
+
+for i in range(n):
+    for j in range(m):
+        h[i][j] = int(h[i][j])
+
+print(max(h))
