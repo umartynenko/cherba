bf0ac64cc72791cb12e58144a148f02df62af82a
umartynenko <uriy.martynenko@gmail.com>
2022-09-23 15:47:35 +0300

    Оптимизация кода

--- test2.py
+++ test2.py
@@ -10,8 +10,11 @@
 # elif n >= 85:
 #     print('Отметка: 5')
 
-m = int(input('MAX количество баллов за тест: '))
-k = int(input('Количество набранных баллов: '))
+title = 'Для выхода из программы введите "-".'
+print('#' * len(title))
+print(title)
+print('#' * len(title))
+print()
 
 
 def get_grade(k, m) -> int:
@@ -26,4 +29,30 @@ def get_grade(k, m) -> int:
         return 5
 
 
-print(f'Отметка: {get_grade(k, m)}')
+m = int(input('MAX количество баллов за тест: '))
+
+dict_students = {}
+while True:
+    name = input('Введите ФИО учащегося: ').title().strip()
+    if name == '-':
+        break
+    else:
+        k = int(input('Количество набранных баллов: '))
+        dict_students[name] = get_grade(m, k)
+
+list_user_name = [i for i in dict_students.keys()]
+list_user_name.sort()
+
+while True:
+    q = input('\nВывести оценки учеников? (+/-): ')
+    if q == '+':
+        print('-' * 50)
+        for i in list_user_name:
+            print(f'{i}: {dict_students[i]}')
+            break
+    elif q.isalpha() or q.isdigit() or q in '!@#$%^&*()?|~`"':
+        print('Вы ввели невероне значение!')
+        continue
+    else:
+        print('-' * 50)
+        break
